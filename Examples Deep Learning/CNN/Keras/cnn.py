4c4ac6f69bd901acff0237f7ab7f4bb0e48dd82f
vmartinezf <vanessa_1895@msn.com>
2018-05-30 21:15:04 +0200

    add keras cnn

---
+++ Examples Deep Learning/CNN/Keras/cnn.py
@@ -0,0 +1,23 @@
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from multi_gpu import make_parallel
+from keras.utils import multi_gpu_model
+import keras
+
+
+def CNN(num_classes, input_shape, ngpus):
+	model = Sequential()
+	model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+	model.add(Conv2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(num_classes, activation='softmax'))
+	#model = make_parallel(model,ngpus)
+	model = multi_gpu_model(model, gpus=2)
+	model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+
+	return model
