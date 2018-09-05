be34721c18516b0e6fa995e277791171562ea1b2
vmartinezf <vanessa_1895@msn.com>
2018-09-05 12:48:16 +0200

    first step with regression

--- Follow Line/First steps/net.py
+++ Follow Line/First steps/net.py
@@ -37,6 +37,42 @@ def process_txt(txt):
 	return data
 
 
+def neural_net_model(X_data, input_dim):
+	"""
+	This function applys 2 hidden layer feed forward neural net.
+	Weights and biases are abberviated as W_1,W_2 and b_1, b_2 
+	These are variables with will be updated during training.
+	"""
+
+	number_of_nodes_in_layer = 10
+
+	# tf.Variable will create a variable of which value will be changing during optimization steps
+	# tf.random_uniform will generate random number of uniform distribution of dimension specified ([input_dim,number_of_nodes_in_layer])
+	W_1 = tf.Variable(tf.random_uniform([input_dim, number_of_nodes_in_layer]))
+	# tf.zeros will create zeros of dimension specified (vector of (1,number_of_hidden_node))
+	b_1 = tf.Variable(tf.zeros([number_of_nodes_in_layer]))
+	# tf.add() will add two parameters
+	# tf.matmul() will multiply two matrices (Weight matrix and input data matrix)
+	layer_1 = tf.add(tf.matmul(X_data,W_1), b_1)
+	# tf.nn.relu() is an activation function  that after multiplication and addition of weights and biases we apply activation function
+	layer_1 = tf.nn.relu(layer_1)
+
+	# layer 1 multiplying and adding bias then activation function
+	W_2 = tf.Variable(tf.random_uniform([number_of_nodes_in_layer, number_of_nodes_in_layer]))
+	b_2 = tf.Variable(tf.zeros([number_of_nodes_in_layer]))
+	layer_2 = tf.add(tf.matmul(layer_1,W_2), b_2)
+	layer_2 = tf.nn.relu(layer_2)
+
+	# layer 2 multiplying and adding bias then activation function
+	W_O = tf.Variable(tf.random_uniform([number_of_nodes_in_layer, 1]))
+	b_O = tf.Variable(tf.zeros([1]))
+	output = tf.add(tf.matmul(layer_2,W_O), b_O)
+
+	# O/p layer multiplying and adding bias then activation function
+	# notice output layer has one node only since performing #regression
+	return output
+
+
 if __name__ == '__main__':
 	data_img = read_images('Dataset/Images/')
 	fileTxt = open('Dataset/angles.txt', 'r')
@@ -53,12 +89,66 @@ if __name__ == '__main__':
 	scaler = MinMaxScaler()
 
 	# We want to predict angle(txt) value
+	# y is output and x is features
 	X_train = scaler.fit_transform(x_train.reshape(-1, 1))
 	y_train = scaler.fit_transform(np.array(y_train).reshape(-1, 1))
 	X_test = scaler.fit_transform(x_test.reshape(-1, 1))
 	y_test = scaler.fit_transform(np.array(y_test).reshape(-1, 1))
 
+	print(x_test[0])
+	print(x_train.shape)
+	print(X_train.shape)
+	print(x_test.shape)
+	print(X_test.shape)
+	print(y_train.shape)
+	print(y_test.shape)
+	print(X_test[0])
+
 	# tf.placeholder() will define gateway for data to graph
 	xs = tf.placeholder("float")
 	ys = tf.placeholder("float")
 
+	output = neural_net_model(xs, 1)
+
+	# Mean squared error cost function
+	cost = tf.reduce_mean(tf.square(output-ys))
+
+	# Gradient Descent optimiztion for updating weights and biases
+	train = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+
+	# Cost of training at each iteration
+	c_t = []
+	# Cost of testing at each iteration
+	c_test = []
+
+	with tf.Session() as sess:
+		# Initiate session and initialize all vaiables
+		sess.run(tf.global_variables_initializer())
+		saver = tf.train.Saver()
+		for i in range(100):
+			for j in range(X_train.shape[0]):
+				# Run cost and train with each sample
+				sess.run([cost,train],feed_dict={xs:X_train[j,:].reshape(1,1), ys:y_train[j]})
+
+			c_t.append(sess.run(cost, feed_dict={xs:X_train,ys:y_train}))
+			c_test.append(sess.run(cost, feed_dict={xs:X_test,ys:y_test}))
+			print('Epoch :',i,'Cost :',c_t[i])
+
+		# Predict output of test data after training
+		pred = sess.run(output, feed_dict={xs:X_test})
+
+		print('Cost :',sess.run(cost, feed_dict={xs:X_test,ys:y_test}))
+
+		# Show results
+		plt.plot(range(y_test.shape[0]),y_test,label="Original Data")
+		plt.plot(range(y_test.shape[0]),pred,label="Predicted Data")
+		plt.legend(loc='best')
+		plt.ylabel('Angle Value')
+		plt.xlabel('Image')
+		plt.title('Angles')
+		plt.show()
+
+		if raw_input('Save model ? [Y/N]: ') == 'Y':
+			# Save model
+			saver.save(sess,'./model.ckpt')
+			print('Model Saved')
