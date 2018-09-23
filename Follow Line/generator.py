b1cce62107f4e0cc85a25d36ef14299ef2a2d1a9
vmartinezf <vanessa_1895@msn.com>
2018-09-23 18:05:12 +0200

    added dataset generator

---
+++ Follow Line/generator.py
@@ -0,0 +1,55 @@
+import os
+import glob
+import json
+
+def create_dataset():
+	# We check if dataset exists
+	foldername = 'Dataset'
+	if not os.path.exists(foldername): os.makedirs(foldername)
+	folder_images = foldername + '/' + 'Images'
+	if not os.path.exists(folder_images): os.makedirs(folder_images)
+
+
+def save_json(v, w):
+	# We save the speed data
+
+	file_name = 'Dataset/data.json'
+
+	data = {
+    	'v': v,
+    	'w': w
+	}
+
+	with open(file_name, 'a') as file:
+		json.dump(data, file)
+
+
+def check_empty_directory(directory):
+	# We check if the directory is empty
+	empty = False
+	for dirName, subdirList, fileList in os.walk(directory):
+		if len(fileList) == 0:
+			empty = True
+
+	return empty
+
+
+def get_number_image(path):
+	list_images = glob.glob(path + '*')
+	sort_images = sorted(list_images, key=lambda x: int(x.split('/')[2].split('.png')[0]))
+	last_number = sort_images[len(sort_images)-1].split('/')[2].split('.png')[0]
+	number = int(last_number)
+	return number
+
+
+def save_image(img):
+	# We save images
+	folder_images = 'Dataset/Images/'
+	empty = check_empty_directory(folder_images)
+	if empty:
+		number = 1
+	else:
+		number = get_number_image(folder_images)
+	name_image = folder_images + str(number) + '.png'
+	cv2.imwrite(name_image, img)
+
