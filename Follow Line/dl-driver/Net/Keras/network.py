1f6b3219719c02252f43e3ae16c199ae562c541a
vmartinezf <vanessa_1895@msn.com>
2018-09-30 23:12:19 +0200

    changes

--- Follow Line/dl-driver/Net/Keras/network.py
+++ Follow Line/dl-driver/Net/Keras/network.py
@@ -1,35 +1,61 @@
 import glob
+import numpy as np
 
+from keras.preprocessing import image
+from sklearn.model_selection import train_test_split
 from models.model_nvidia import nvidia_model
 
 
 def parse_json(data):
-    array_v = []
-    array_w = []
+    array = []
     # We process json
     data_parse = data.split('}')[:-1]
     for d in data_parse:
         v = d.split('"v":')[1]
-        array_v.append(int(v))
         d_parse = d.split(', "v":')[0]
         w = d_parse.split((': '))[1]
-        array_w.append(float(w))
+        array.append([int(v), float(w)])
 
-    return array_v, array_w
+    return array
+
+
+def get_images(list_images):
+    array_imgs = []
+
+    for name in list_images:
+        # This is a PIL image
+        print(name)
+        img = image.load_img(name)
+        print('load')
+        # This is a Numpy array
+        img = image.img_to_array(img)
+        print('array')
+        array_imgs.append(img)
+
+    return array_imgs
 
 
 if __name__ == "__main__":
+    seed = 7
+    np.random.seed(seed)
 
     # Load data
     list_images = glob.glob('../Dataset/Images/' + '*')
-    list_images = sorted(list_images, key=lambda x: int(x.split('/')[3].split('.png')[0]))
+    images = sorted(list_images, key=lambda x: int(x.split('/')[3].split('.png')[0]))
 
     file = open('../Dataset/data.json', 'r')
     data = file.read()
     file.close()
 
-    # We process json
-    array_v, array_w = parse_json(data)
+    # We preprocess images
+    x = get_images(images)
+    # We preprocess json
+    y = parse_json(data)
+
+    print(len(x), len(y))
+
+    # Split data into 67% for train and 33% for test
+    #X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=seed)
 
     # Variables
     batch_size = 128
@@ -37,3 +63,5 @@ if __name__ == "__main__":
     img_shape = (640, 480, 3)
 
     model = nvidia_model(img_shape)
+
+    #print(len(X_train), len(X_test), len(y_train), len(y_test))
