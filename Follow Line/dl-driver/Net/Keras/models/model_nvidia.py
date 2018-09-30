fed6de8a78bab6495c40bba6b7fa1c3e802fc722
vmartinezf <vanessa_1895@msn.com>
2018-09-30 16:35:48 +0200

    added model

---
+++ Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
@@ -0,0 +1,23 @@
+# Based on: https://arxiv.org/pdf/1604.07316.pdf
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Conv2D
+
+
+def nvidia_model(img_shape):
+    '''
+    Model of End to End Learning for Self-Driving Cars (NVIDIA)
+    '''
+    model = Sequential()
+    model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", input_shape=img_shape))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(50, activation="relu"))
+    model.add(Dense(10, activation="relu"))
+    model.add(Dense(1))
+    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+    return model
