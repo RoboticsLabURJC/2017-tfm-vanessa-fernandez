fa6c631d915ff1e7536582ae9d922d87a4118083
vmartinezf <vanessa_1895@msn.com>
2018-10-03 12:51:57 +0200

    added classification

---
+++ Follow Line/add_classification_data.py
@@ -0,0 +1,28 @@
+# This file modify json data if it hasn't the classification data
+
+
+def get_classification(data):
+	v = d.split('"v":')[1]
+	d_parse = d.split(', "v":')[0]
+	w = d_parse.split((': '))[1]
+	if w[0] == '-':
+		classification = 'right'
+	else:
+		classification = 'left'
+	return classification
+
+
+if __name__ == "__main__":
+	filename = 'Dataset/data.json'
+	output = ''
+
+	with open(filename, "r+") as f:
+		data = f.read()
+		data_parse = data.split('}')[:-1]
+		for d in data_parse:
+			classification = get_classification(d)
+			output = output + d + ', "classification": ' + classification + '}'
+			f.seek(0)
+			f.write(output)
+			f.truncate()
+
