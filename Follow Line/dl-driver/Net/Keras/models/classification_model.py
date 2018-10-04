4869b867037a4dbf466e68608e2e85e8bee95f22
vmartinezf <vanessa_1895@msn.com>
2018-10-04 16:32:03 +0200

    modified model

--- Follow Line/dl-driver/Net/Keras/models/classification_model.py
+++ Follow Line/dl-driver/Net/Keras/models/classification_model.py
@@ -2,15 +2,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 
 
-def cnn_model(num_classes, input_shape):
+def cnn_model(input_shape):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Conv2D(32, (3, 3), input_shape = input_shape, activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.add(Dense(units = 128, activation = 'relu'))
+    model.add(Dense(units = 1, activation = 'sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
