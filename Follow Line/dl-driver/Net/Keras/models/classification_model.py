50074a10de2eecf7b54f99fd07ba710a3ca3235e
vmartinezf <vanessa_1895@msn.com>
2018-10-06 17:32:00 +0200

    modified model

--- Follow Line/dl-driver/Net/Keras/models/classification_model.py
+++ Follow Line/dl-driver/Net/Keras/models/classification_model.py
@@ -1,13 +1,28 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation
 
 
 def cnn_model(input_shape):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape = input_shape, activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
-    model.add(Dense(units = 128, activation = 'relu'))
-    model.add(Dense(units = 1, activation = 'sigmoid'))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
     return model
