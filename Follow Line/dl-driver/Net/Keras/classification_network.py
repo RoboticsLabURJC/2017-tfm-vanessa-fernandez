7a0e857b85333d3083653de3f63ed2d4e22921a3
vmartinezf <vanessa_1895@msn.com>
2018-10-08 16:58:43 +0200

    added class ClassificationNetwork

---
+++ Follow Line/dl-driver/Net/Keras/classification_network.py
@@ -0,0 +1,51 @@
+#
+#  Authors :
+#       Vanessa Fernandez Martinez <vanessa_1895@msn.com>
+
+import cv2
+import numpy as np
+
+from keras.models import load_model
+
+
+class ClassificationNetwork():
+    def __init__(self, net_model):
+        self.model_file = 'Net/Keras/' + net_model['Model']
+        self.model = load_model(self.model_file)
+
+        # the Keras network works on 320x240
+        self.img_height = 240
+        self.img_width = 320
+
+        self.prediction = ""
+
+
+    def setCamera(self, camera):
+        self.camera = camera
+
+
+    def convertLabel(self, label):
+        if label == 0:
+            string_label = "left"
+        else:
+            string_label = "right"
+        return string_label
+
+
+    def predict(self):
+        input_image = self.camera.getImage()
+
+        # Preprocessing
+        img_resized = cv2.resize(input_image.data, (self.img_width, self.img_height))
+
+        # We adapt the image
+        input_img = np.stack([img_resized], axis=0)
+
+        # Make prediction
+        prediction = self.model.predict(input_img)
+        y_pred = int(prediction[0])
+
+        # Convert int prediction to corresponded label
+        y_pred = self.convertLabel(y_pred)
+
+        self.prediction = y_pred
