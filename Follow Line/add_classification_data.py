1b6fb539a83cc14ba2275cae433593a8ab0c727d
vmartinezf <vanessa_1895@msn.com>
2018-10-12 19:43:14 +0200

    added new class

--- Follow Line/add_classification_data.py
+++ Follow Line/add_classification_data.py
@@ -1,14 +1,28 @@
 # This file modify json data if it hasn't the classification data
 
 
-def get_classification(data):
+def get_w(data):
 	v = data.split('"v":')[1]
 	d_parse = data.split(', "v":')[0]
-	w = d_parse.split((': '))[1]
-	if w[0] == '-':
-		classification = 'right'
+	w = d_parse.split((', "w": '))[1]
+	return w
+
+
+def get_classification(w):
+	if w[0] == '-' and abs(float(w)) >= 1.0:
+		classification = 'radically_right'
+	elif w[0] == '-' and abs(float(w)) >= 0.5:
+		classification = 'moderately_right'
+	elif w[0] == '-' and abs(float(w)) >= 0.1:
+		classification = 'slightly_right'
+	elif abs(float(w)) >= 1.0:
+		classification = 'radically_left'
+	elif abs(float(w)) >= 0.5:
+		classification = 'moderately_left'
+	elif abs(float(w)) >= 0.1:
+		classification = 'slightly_left'
 	else:
-		classification = 'left'
+		classification = 'slight'
 	return classification
 
 
@@ -21,8 +35,9 @@ if __name__ == "__main__":
 		data_parse = data.split('{')[1:]
 
 		for d in data_parse:
-			classification = get_classification(d)
-			output = output + '{"classification": ' + classification + ', ' + d
+			w = get_w(d)
+			classification = get_classification(w)
+			output = output + '{"class2": ' + classification + ', ' + d
 
 		f.seek(0)
 		f.write(output)
