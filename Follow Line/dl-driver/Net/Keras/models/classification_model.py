c40449a868a214b2032fc478ea4cd3a597bda53a
vmartinezf <vanessa_1895@msn.com>
2018-10-18 10:00:17 +0200

    modified model

--- Follow Line/dl-driver/Net/Keras/models/classification_model.py
+++ Follow Line/dl-driver/Net/Keras/models/classification_model.py
@@ -1,5 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation
+from keras.optimizers import SGD
+
 
 
 def cnn_model(input_shape):
@@ -24,5 +26,27 @@ def cnn_model(input_shape):
     model.add(Activation('sigmoid'))
 
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+    return model
+
+
+def lenet5(input_shape, num_classes):
+    model = Sequential()
+
+    model.add(Conv2D(32, (5, 5), padding='same', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(32, (5, 5), padding='same', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+
+    model.add(Dense(units=512, activation='relu'))
+    model.add(Dense(units=num_classes, activation='softmax'))
+
+    optimizer = SGD(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
     return model
