200deddc9890d85f6b94675d382451d27e25dab8
vmartinezf <vanessa_1895@msn.com>
2018-10-18 15:14:59 +0200

    smaller vgg

--- Follow Line/dl-driver/Net/Keras/classification_network.py
+++ Follow Line/dl-driver/Net/Keras/classification_network.py
@@ -18,9 +18,11 @@ class ClassificationNetwork():
         # Obtain the graph
         self.graph = tf.get_default_graph()
 
-        # The Keras network works on 320x240
-        self.img_height = 240
-        self.img_width = 320
+        # The Keras network works on 160x120
+        self.img_height = 120
+        self.img_width = 160
+
+        self.num_classes = 7
 
         self.prediction = ""
 
@@ -30,10 +32,26 @@ class ClassificationNetwork():
 
 
     def convertLabel(self, label):
-        if label == 0:
-            string_label = "left"
-        else:
-            string_label = "right"
+        if self.num_classes == 2:
+            if label == 0:
+                string_label = "left"
+            else:
+                string_label = "right"
+        elif self.num_classes == 7:
+            if label == 0:
+                string_label = "radically_left"
+            elif label == 1:
+                string_label = "moderately_left"
+            elif label == 2:
+                string_label = "slightly_left"
+            elif label == 3:
+                string_label = "slight"
+            elif label == 4:
+                string_label = "slightly_right"
+            elif label == 5:
+                string_label = "moderately_right"
+            elif label == 6:
+                string_label = "radically_right"
         return string_label
 
 
@@ -49,8 +67,8 @@ class ClassificationNetwork():
         # While predicting, use the same graph
         with self.graph.as_default():
             # Make prediction
-            prediction = self.model.predict(input_img)
-        y_pred = int(prediction[0])
+            predictions = self.model.predict(input_img)
+        y_pred = [np.argmax(prediction) for prediction in predictions][0]
 
         # Convert int prediction to corresponded label
         y_pred = self.convertLabel(y_pred)
