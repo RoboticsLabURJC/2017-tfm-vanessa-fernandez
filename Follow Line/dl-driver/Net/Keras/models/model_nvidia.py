67ae379f0880242ff06b1286f45128f05b338234
vmartinezf <vanessa_1895@msn.com>
2018-10-21 18:29:22 +0200

    model pilotnet

--- Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
+++ Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Conv2D
 
 
-def nvidia_model(img_shape):
+def pilotnet_model(img_shape):
     '''
     Model of End to End Learning for Self-Driving Cars (NVIDIA)
     '''
@@ -15,6 +15,7 @@ def nvidia_model(img_shape):
     model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
     model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
     model.add(Flatten())
+    model.add(Dense(1164, activation="relu"))
     model.add(Dense(100, activation="relu"))
     model.add(Dense(50, activation="relu"))
     model.add(Dense(10, activation="relu"))
