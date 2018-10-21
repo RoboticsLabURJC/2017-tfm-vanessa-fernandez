5f1b3e8680d2202500076c1fb8370d6aa9688391
vmartinezf <vanessa_1895@msn.com>
2018-10-21 20:11:05 +0200

    modified train

--- Follow Line/dl-driver/Net/Keras/train.py
+++ Follow Line/dl-driver/Net/Keras/train.py
@@ -10,17 +10,18 @@ from models.model_nvidia import pilotnet_model
 
 
 def parse_json(data):
-    array = []
+    array_v = []
+    array_w = []
     # We process json
     data_parse = data.split('}')[:-1]
     for d in data_parse:
         v = d.split('"v": ')[1]
         d_parse = d.split(', "v":')[0]
         w = d_parse.split(('"w": '))[1]
-        #array.append([float(v), float(w)])
-        array.append(float(w))
+        array_v.append(float(v))
+        array_w.append(float(w))
 
-    return array
+    return array_v, array_w
 
 
 def get_images(list_images):
@@ -32,20 +33,6 @@ def get_images(list_images):
         array_imgs.append(img)
 
     return array_imgs
-
-
-# def adapt_array(array):
-#     new_array = []
-#     num_array = 300
-#     for i in range(0, num_array):
-#         if i == 0:
-#             array_split = array[:len(array)/num_array]
-#         elif i == num_array - 1:
-#             array_split = array[i*len(array)/num_array:]
-#         else:
-#             array_split = array[i*len(array)/num_array:len(array)*(i+1)/num_array]
-#         new_array.append(array_split)
-#     return new_array
 
 
 if __name__ == "__main__":
@@ -61,80 +48,80 @@ if __name__ == "__main__":
     # We preprocess images
     x = get_images(images)
     # We preprocess json
-    y = parse_json(data)
+    y_v, y_w = parse_json(data)
 
     # Split data into 80% for train and 20% for validation
-    X_train, X_validation, y_train, y_validation = train_test_split(x, y, test_size=0.20, random_state=42)
-
-    # # Split a array in a array with small arrays
-    # array_x_train = adapt_array(X_train)
-    # array_y_train = adapt_array(y_train)
-    # array_x_test = adapt_array(X_validation)
-    # array_y_test = adapt_array(y_validation)
+    X_train_v, X_validation_v, y_train_v, y_validation_v = train_test_split(x, y_v, test_size=0.20, random_state=42)
+    X_train_w, X_validation_w, y_train_w, y_validation_w = train_test_split(x, y_w, test_size=0.20, random_state=42)
 
     # Variables
     batch_size = 64
-    nb_epoch = 1000
+    nb_epoch = 180
     img_shape = (120, 160, 3)
 
     # Get model
-    model = pilotnet_model(img_shape)
+    model_v = pilotnet_model(img_shape)
+    model_w = pilotnet_model(img_shape)
     model_png = 'models/model_pilotnet.png'
 
-    X_train = np.stack(X_train, axis=0)
-    y_train = np.stack(y_train, axis=0)
-    X_validation = np.stack(X_validation, axis=0)
-    y_validation = np.stack(y_validation, axis=0)
+    # We adapt the data
+    X_train_v = np.stack(X_train_v, axis=0)
+    y_train_v = np.stack(y_train_v, axis=0)
+    X_validation_v = np.stack(X_validation_v, axis=0)
+    y_validation_v = np.stack(y_validation_v, axis=0)
+
+    X_train_w = np.stack(X_train_w, axis=0)
+    y_train_w = np.stack(y_train_w, axis=0)
+    X_validation_w = np.stack(X_validation_w, axis=0)
+    y_validation_w = np.stack(y_validation_w, axis=0)
 
     # Print layers
-    print(model.summary())
+    print(model_v.summary())
     # Plot layers of model
-    plot_model(model, to_file=model_png)
+    plot_model(model_v, to_file=model_png)
 
     #  We train
-    model_history = model.fit(X_train, y_train, epochs=nb_epoch, batch_size=batch_size, verbose=2,
-                              validation_data=(X_validation, y_validation))
+    model_history_v = model_v.fit(X_train_v, y_train_v, epochs=nb_epoch, batch_size=batch_size, verbose=2,
+                              validation_data=(X_validation_v, y_validation_v))
+
+    model_history_w = model_w.fit(X_train_w, y_train_w, epochs=nb_epoch, batch_size=batch_size, verbose=2,
+                                  validation_data=(X_validation_w, y_validation_w))
 
     # We evaluate the model
-    score = model.evaluate(X_validation, y_validation, verbose=0)
+    score = model_v.evaluate(X_validation_v, y_validation_v, verbose=0)
+    print(score)
+    print('Test loss: ', score[0])
+    print('Test accuracy: ', score[1])
+    print('Test mean squared error: ', score[2])
+    print('Test mean absolute error: ', score[3])
+
+    score = model_w.evaluate(X_validation_w, y_validation_w, verbose=0)
     print(score)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
-
-    # for i in range(0, len(array_x_train)):
-    #     # We adapt the data
-    #     X_train = np.stack(array_x_train[i], axis=0)
-    #     y_train = np.stack(array_y_train[i], axis=0)
-    #     X_test = np.stack(array_x_test[i], axis=0)
-    #     y_test = np.stack(array_y_test[i], axis=0)
-    #
-    #     # We train
-    #     model_history = model.fit(X_train, y_train, epochs=nb_epoch, validation_split=0.2, batch_size=batch_size)
-    #
-    #     # We evaluate the model
-    #     score = model.evaluate(X_test, y_test, verbose=0)
-    #     print('Test loss:', score[0])
-    #     print('Test accuracy:', score[1])
+    print('Test mean squared error: ', score[2])
+    print('Test mean absolute error: ', score[3])
 
     # We save the model
-    model.save('models/model_pilotnet.h5')
+    model_v.save('models/model_pilotnet_v.h5')
+    model_w.save('models/model_pilotnet_w.h5')
 
     # Plot the training and validation loss for each epoch
-    plt.plot(model_history.history['loss'])
-    plt.plot(model_history.history['val_loss'])
-    plt.title('mse')
-    plt.ylabel('mean squared error loss')
-    plt.xlabel('epoch')
-    plt.legend(['training set', 'validation set'], loc='upper right')
-    plt.ylim([0, 0.1])
-    plt.show()
-
-    # Accuracy Curves
-    plt.figure(figsize=[8, 6])
-    plt.plot(model_history.history['acc'], 'r', linewidth=3.0)
-    plt.plot(model_history.history['val_acc'], 'b', linewidth=3.0)
-    plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
-    plt.xlabel('Epochs ', fontsize=16)
-    plt.ylabel('Accuracy', fontsize=16)
-    plt.title('Accuracy Curves', fontsize=16)
-    plt.show()
+    # plt.plot(model_history.history['loss'])
+    # plt.plot(model_history.history['val_loss'])
+    # plt.title('mse')
+    # plt.ylabel('mean squared error loss')
+    # plt.xlabel('epoch')
+    # plt.legend(['training set', 'validation set'], loc='upper right')
+    # plt.ylim([0, 0.1])
+    # plt.show()
+    #
+    # # Accuracy Curves
+    # plt.figure(figsize=[8, 6])
+    # plt.plot(model_history.history['acc'], 'r', linewidth=3.0)
+    # plt.plot(model_history.history['val_acc'], 'b', linewidth=3.0)
+    # plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
+    # plt.xlabel('Epochs ', fontsize=16)
+    # plt.ylabel('Accuracy', fontsize=16)
+    # plt.title('Accuracy Curves', fontsize=16)
+    # plt.show()
