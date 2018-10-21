d0bc152c0a994066d54850c8f61d80db43819087
vmartinezf <vanessa_1895@msn.com>
2018-10-21 18:43:31 +0200

    modified train

--- Follow Line/dl-driver/Net/Keras/train.py
+++ Follow Line/dl-driver/Net/Keras/train.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from models.model_nvidia import nvidia_model
+from models.model_nvidia import pilotnet_model
 
 
 def parse_json(data):
@@ -15,7 +15,8 @@ def parse_json(data):
         v = d.split('"v": ')[1]
         d_parse = d.split(', "v":')[0]
         w = d_parse.split(('"w": '))[1]
-        #array.append([int(v), float(w)])
+        print(w)
+        #array.append([float(v), float(w)])
         array.append(float(w))
 
     return array
@@ -26,24 +27,24 @@ def get_images(list_images):
     array_imgs = []
     for name in list_images:
         img = cv2.imread(name)
-        img = cv2.resize(img, (img.shape[1] / 2, img.shape[0] / 2))
+        img = cv2.resize(img, (img.shape[1] / 4, img.shape[0] / 4))
         array_imgs.append(img)
 
     return array_imgs
 
 
-def adapt_array(array):
-    new_array = []
-    num_array = 300
-    for i in range(0, num_array):
-        if i == 0:
-            array_split = array[:len(array)/num_array]
-        elif i == num_array - 1:
-            array_split = array[i*len(array)/num_array:]
-        else:
-            array_split = array[i*len(array)/num_array:len(array)*(i+1)/num_array]
-        new_array.append(array_split)
-    return new_array
+# def adapt_array(array):
+#     new_array = []
+#     num_array = 300
+#     for i in range(0, num_array):
+#         if i == 0:
+#             array_split = array[:len(array)/num_array]
+#         elif i == num_array - 1:
+#             array_split = array[i*len(array)/num_array:]
+#         else:
+#             array_split = array[i*len(array)/num_array:len(array)*(i+1)/num_array]
+#         new_array.append(array_split)
+#     return new_array
 
 
 if __name__ == "__main__":
@@ -72,11 +73,11 @@ if __name__ == "__main__":
 
     # Variables
     batch_size = 32
-    nb_epoch = 15
-    img_shape = (240, 320, 3)
+    nb_epoch = 2000
+    img_shape = (120, 160, 3)
 
     # Get model
-    model = nvidia_model(img_shape)
+    model = pilotnet_model(img_shape)
 
     X_train = np.stack(X_train, axis=0)
     y_train = np.stack(y_train, axis=0)
