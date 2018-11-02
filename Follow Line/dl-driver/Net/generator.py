a929e93f6abfe24215be323163fddda69685c320
vmartinezf <vanessa_1895@msn.com>
2018-11-02 12:47:04 +0100

    added classes

--- Follow Line/dl-driver/Net/generator.py
+++ Follow Line/dl-driver/Net/generator.py
@@ -19,17 +19,51 @@ def get_classification(w):
 	return classification
 
 
+def get_classification_w(w):
+	if w[0] == '-' and abs(float(w)) >= 1.0:
+		classification = 'radically_right'
+	elif w[0] == '-' and abs(float(w)) >= 0.5:
+		classification = 'moderately_right'
+	elif w[0] == '-' and abs(float(w)) >= 0.1:
+		classification = 'slightly_right'
+	elif abs(float(w)) >= 1.0:
+		classification = 'radically_left'
+	elif abs(float(w)) >= 0.5:
+		classification = 'moderately_left'
+	elif abs(float(w)) >= 0.1:
+		classification = 'slightly_left'
+	else:
+		classification = 'slight'
+	return classification
+
+
+def get_classification_v(v):
+	if v > 11:
+		classification = 'very_fast'
+	elif v > 9:
+		classification = 'fast'
+	elif v > 7:
+		classification = 'moderate'
+	else:
+		classification = 'slow'
+	return classification
+
+
 def save_json(v, w):
 	# We save the speed data
 
 	file_name = 'Net/Dataset/data.json'
 
 	classification = get_classification(w)
+	classification_w = get_classification_w(w)
+	classification_v = get_classification_v(v)
 
 	data = {
     	'v': v,
     	'w': w,
-		'classification': classification
+		'classification': classification,
+		'class2': classification_w,
+		'class3': classification_v
 	}
 
 	with open(file_name, 'a') as file:
