3e62af731b5781792b5213fdf8c47a9e902bb1ed
vmartinezf <vanessa_1895@msn.com>
2018-11-06 15:47:07 +0100

    added v classification

--- Follow Line/dl-driver/Net/Keras/classification_network.py
+++ Follow Line/dl-driver/Net/Keras/classification_network.py
@@ -12,8 +12,10 @@ from keras.models import load_model
 class ClassificationNetwork():
     def __init__(self, net_model):
         # Load model
-        self.model_file = 'Net/Keras/' + net_model['Model_Classification']
-        self.model = load_model(self.model_file)
+        self.model_file_v = 'Net/Keras/' + net_model['Model_Classification_v']
+        self.model_file_w = 'Net/Keras/' + net_model['Model_Classification_w']
+        self.model_v = load_model(self.model_file_v)
+        self.model_w = load_model(self.model_file_w)
 
         # Obtain the graph
         self.graph = tf.get_default_graph()
@@ -22,22 +24,23 @@ class ClassificationNetwork():
         self.img_height = 120
         self.img_width = 160
 
-        self.num_classes = 7
+        self.num_classes_w = 7
 
-        self.prediction = ""
+        self.prediction_v = ""
+        self.prediction_w = ""
 
 
     def setCamera(self, camera):
         self.camera = camera
 
 
-    def convertLabel(self, label):
-        if self.num_classes == 2:
+    def convertLabel_w(self, label):
+        if self.num_classes_w == 2:
             if label == 0:
                 string_label = "left"
             else:
                 string_label = "right"
-        elif self.num_classes == 7:
+        elif self.num_classes_w == 7:
             if label == 0:
                 string_label = "radically_left"
             elif label == 1:
@@ -55,6 +58,18 @@ class ClassificationNetwork():
         return string_label
 
 
+    def convertLabel_v(self, label):
+        if label == 0:
+            string_label = "slow"
+        elif label == 1:
+            string_label = "moderate"
+        elif label == 2:
+            string_label = "fast"
+        elif label == 3:
+            string_label = "very_fast"
+        return string_label
+
+
     def predict(self):
         input_image = self.camera.getImage()
 
@@ -67,10 +82,14 @@ class ClassificationNetwork():
         # While predicting, use the same graph
         with self.graph.as_default():
             # Make prediction
-            predictions = self.model.predict(input_img)
-        y_pred = [np.argmax(prediction) for prediction in predictions][0]
+            predictions_v = self.model_v.predict(input_img)
+            predictions_w = self.model_w.predict(input_img)
+        y_pred_v = [np.argmax(prediction) for prediction in predictions_v][0]
+        y_pred_w = [np.argmax(prediction) for prediction in predictions_w][0]
 
         # Convert int prediction to corresponded label
-        y_pred = self.convertLabel(y_pred)
+        y_pred_v = self.convertLabel_v(y_pred_v)
+        y_pred_w = self.convertLabel_w(y_pred_w)
 
-        self.prediction = y_pred
+        self.prediction_v = y_pred_v
+        self.prediction_w = y_pred_w
