d09f9946e9f7e5cf3421d75a6bbc1ef9ea917536
vmartinezf <vanessa_1895@msn.com>
2018-11-13 15:47:33 +0100

    modified file

--- Follow Line/save_differences.py
+++ Follow Line/save_differences.py
@@ -8,6 +8,8 @@ from skimage.measure import compare_ssim, compare_mse
 def compare_images(list_images_dataset, img_driving):
 	# We initialize variables
 	init = True
+	sum_ssim = 0
+	sum_mse = 0
 
 	for name in list_images_dataset:
 		img = cv2.imread(name)
@@ -16,6 +18,9 @@ def compare_images(list_images_dataset, img_driving):
 		# We compare imagess
 		ssim = compare_ssim(img, img_driving, multichannel=True, full=True)
 		mse = compare_mse(img, img_driving)
+
+		sum_ssim += ssim[0]
+		sum_mse += mse
 
 		if init:
 			init = False
@@ -44,8 +49,11 @@ def compare_images(list_images_dataset, img_driving):
 				min_ssim = ssim
 				name_min_ssim = name
 
-	print(name_max_mse, name_min_mse)
-	print(name_min_ssim, name_max_ssim)
+	mean_mse = sum_mse / len(list_images_dataset)
+	mean_ssim = sum_ssim / len(list_images_dataset)
+	print(name_max_mse, name_min_mse, mean_mse)
+	print(name_min_ssim, name_max_ssim, mean_ssim)
+	return name_max_mse, name_min_mse, mean_mse, name_max_ssim, name_min_ssim, mean_ssim
 
 
 if __name__ == "__main__":
@@ -54,8 +62,14 @@ if __name__ == "__main__":
 	list_images_dataset = glob.glob('Dataset/Train/Images/' + '*')
 	images_dataset = sorted(list_images_dataset, key=lambda x: int(x.split('/')[3].split('.png')[0]))
 
+	file_txt = open('Failed_driving/measures.txt', 'w')
+	file_txt.write('Image_max_mse Image_min_mse Mean_mse Image_max_ssim Image_min_ssim Mean_ssim\n')
+
 	for name in images_driving:
 		img_driving = cv2.imread(name)
 		img_driving = cv2.cvtColor(img_driving, cv2.COLOR_BGR2RGB)
-		compare_images(images_dataset, img_driving)
-		i += 1
+		name_max_mse, name_min_mse, mean_mse, name_max_ssim, name_min_ssim, mean_ssim = compare_images(images_dataset, img_driving)
+		file_txt.write(name_max_mse + ' ' + name_min_mse + ' ' + str(mean_mse) + ' ' + name_max_ssim + ' ' + name_min_ssim + ' ' + str(mean_ssim) + '\n')
+		
+	file_txt.close()
+
