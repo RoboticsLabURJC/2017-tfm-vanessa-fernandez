faab67ec064b67380e872e497354ff7a6b899e86
vmartinezf <vanessa_1895@msn.com>
2018-12-11 09:36:41 +0100

    cnn+lstm

--- Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
+++ Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
@@ -4,18 +4,8 @@
 #       Vanessa Fernandez Martinez <vanessa_1895@msn.com>
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, BatchNormalization, Dropout
+from keras.layers import Flatten, Dense, Conv2D, BatchNormalization, Dropout, ConvLSTM2D
 from keras.optimizers import Adam
-import keras.backend as K
-
-
-# def myAccuracy_regression(y_true, y_pred):
-#     # Absolute difference between correct and predicted values
-#     diff = K.abs(y_true-y_pred)
-#     # Tensor with 0 for false values and 1 for true values
-#     correct = K.less(diff,0.05)
-#     # Sum all 1's and divide by the total
-#     return K.mean(correct)
 
 
 def pilotnet_model(img_shape):
@@ -49,6 +39,21 @@ def tinypilotnet_model(img_shape):
     model.add(Flatten())
     model.add(Dense(50, activation="relu"))
     model.add(Dense(10, activation="relu"))
+    model.add(Dense(1))
+    adam = Adam(lr=0.0001)
+    model.compile(optimizer=adam, loss="mse", metrics=['accuracy', 'mse', 'mae'])
+    return model
+
+
+def lstm_tinypilotnet_model(img_shape):
+    model = Sequential()
+    model.add(BatchNormalization(epsilon=0.001, axis=-1, input_shape=img_shape))
+    model.add(Conv2D(8, (3, 3), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(16, (3, 3), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(32, (3, 3), strides=(2, 2), activation="relu"))
+    model.add(ConvLSTM2D(nb_filter=40, nb_row=3, nb_col=3, border_mode='same', return_sequences=True))
+    model.add(Conv2D(1, (3, 3), strides=(2, 2), activation="relu"))
+    model.add(Flatten())
     model.add(Dense(1))
     adam = Adam(lr=0.0001)
     model.compile(optimizer=adam, loss="mse", metrics=['accuracy', 'mse', 'mae'])
