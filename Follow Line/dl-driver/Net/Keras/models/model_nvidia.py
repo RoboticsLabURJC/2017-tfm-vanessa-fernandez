bb3fa00ae910f41582e9f322a1e75a54f75107ca
vmartinezf <vanessa_1895@msn.com>
2019-01-09 18:05:34 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
+++ Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
@@ -115,7 +115,7 @@ def lstm_model(img_shape):
 
     model.add(Lambda(
         lambda x: x / 127.5 - 1.,
-        batch_input_shape=(1734.1, 10, img_shape[0], img_shape[1], img_shape[2]),
+        batch_input_shape=(1734, 10, img_shape[0], img_shape[1], img_shape[2]),
     ))
     model.add(TimeDistributed(Conv2D(24, (5, 5), init="he_normal", activation='relu', subsample=(5, 4),
                                      border_mode='valid')))#, input_shape=img_shape))
