4f0675e845749722e01685d7fde468a1b1c854ce
vmartinezf <vanessa_1895@msn.com>
2019-01-09 13:06:47 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/train.py
+++ Follow Line/dl-driver/Net/Keras/train.py
@@ -82,12 +82,10 @@ if __name__ == "__main__":
     X_validation_v = np.stack(X_validation_v, axis=0)
     y_validation_v = np.stack(y_validation_v, axis=0)
     print(X_train_v.shape)
-    X_train_v = np.reshape(X_train_v, (len(X_train_v), 10, img_shape[0], img_shape[1], img_shape[2])).swapaxes(0,2)
-    y_train_v = np.reshape(y_train_v, (len(X_train_v), 10, img_shape[0], img_shape[1], img_shape[2])).swapaxes(0,2)
-    X_validation_v = np.reshape(X_validation_v, (len(X_validation_v), 10, img_shape[0],
-                                                 img_shape[1], img_shape[2])).swapaxes(0,2)
-    y_validation_v = np.reshape(y_validation_v, (len(X_validation_v), 10, img_shape[0],
-                                                 img_shape[1], img_shape[2])).swapaxes(0,2)
+    X_train_v = np.reshape(X_train_v, (len(X_train_v), 10, img_shape[0], img_shape[1], img_shape[2]))
+    y_train_v = np.reshape(y_train_v, (len(X_train_v), 10, img_shape[0], img_shape[1], img_shape[2]))
+    X_validation_v = np.reshape(X_validation_v, (len(X_validation_v), 10, img_shape[0], img_shape[1], img_shape[2]))
+    y_validation_v = np.reshape(y_validation_v, (len(X_validation_v), 10, img_shape[0], img_shape[1], img_shape[2]))
     print(X_train_v.shape)
 
 
