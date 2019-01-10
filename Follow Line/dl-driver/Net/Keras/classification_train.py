d8d3087d1decb9d1a9471ca4eebb6b30bb886aca
vmartinezf <vanessa_1895@msn.com>
2019-01-10 11:19:15 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/classification_train.py
+++ Follow Line/dl-driver/Net/Keras/classification_train.py
@@ -192,7 +192,6 @@ def choose_model(name, input_shape, num_classes, name_variable, type_net):
             batch_size = 64
             nb_epochs = 35
             if type_net == "biased":
-                nb_epochs = 45
                 class_weight = {0: 4., 1: 2., 2: 2., 3: 1., 4:2., 5: 2., 6: 3.}
             else:
                 class_weight = None
