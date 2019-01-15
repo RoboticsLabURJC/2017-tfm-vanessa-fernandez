1530e9947f35aa6fad68d33dbc58ae0e0655ee09
vmartinezf <vanessa_1895@msn.com>
2019-01-15 17:24:37 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/train.py
+++ Follow Line/dl-driver/Net/Keras/train.py
@@ -101,10 +101,10 @@ if __name__ == "__main__":
         X_train_v, X_validation_v, y_train_v, y_validation_v = train_test_split(x, y_v, test_size=0.20, random_state=42)
         X_train_w, X_validation_w, y_train_w, y_validation_w = train_test_split(x, y_w, test_size=0.20, random_state=42)
     elif type_net == 'lstm_tinypilotnet' or type_net == 'lstm':
-        X_train_v = x[:100]
-        X_train_w = x[:100]
-        y_train_v = y_v[:100]
-        y_train_w = y_w[:100]
+        X_train_v = x
+        X_train_w = x
+        y_train_v = y_v
+        y_train_w = y_w
         X_t_v, X_validation_v, y_t_v, y_validation_v = train_test_split(x, y_v, test_size=0.20, random_state=42)
         X_t_w, X_validation_w, y_t_w, y_validation_w = train_test_split(x, y_w, test_size=0.20, random_state=42)
 
