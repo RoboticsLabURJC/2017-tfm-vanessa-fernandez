4badb08acc6201143f82c6379f38e23954c45a2f
vmartinezf <vanessa_1895@msn.com>
2019-02-08 10:26:51 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/train.py
+++ Follow Line/dl-driver/Net/Keras/train.py
@@ -156,9 +156,9 @@ if __name__ == "__main__":
     y_v, y_w = parse_json(data)
 
     if type_net == 'lstm':
-        x = x[:10000]
-        y_v = y_v[:10000]
-        y_w = y_w[:10000]
+        x = x[:5000]
+        y_v = y_v[:5000]
+        y_w = y_w[:5000]
 
     # Split data into 80% for train and 20% for validation
     if type_net == 'pilotnet' or type_net == 'tinypilotnet':
