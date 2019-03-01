fe9ef151aa626909ac2d1a1f3361af0ac28161af
vmartinezf <vanessa_1895@msn.com>
2019-03-01 12:52:35 +0100

    changes

--- Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
+++ Follow Line/dl-driver/Net/Keras/models/model_nvidia.py
@@ -144,11 +144,12 @@ def controlnet_model(img_shape):
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dense(50, activation="relu"))
     model.add(Dense(50, activation="relu"))
-    print(model.summary())
     model.add(Reshape((2, 50)))
     model.add(LSTM(5))
+    model.add(Reshape((5,)))
     print(model.summary())
     model.add(Activation('softmax'))
+    model.add(Dense(1))
     adam = Adam(lr=0.0001)
     model.compile(optimizer=adam, loss="mse", metrics=['accuracy', 'mse', 'mae'])
     return model
